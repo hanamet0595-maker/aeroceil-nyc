d99d5c2e399c0ba4b8cba91486a45d61526ed78e
hanamet0595-maker <han.amet0595@gmail.com>
2025-08-12 19:41:22 -0400

    Create App.jsx

---
+++ rc/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1 style={{ margin: 0 }}>AeroCeil NYC</h1>
+      <p>
+        Deployment test. If you see this page, the build succeeded.
+        Next step: replace this file with the full luxury version.
+      </p>
+    </div>
+  );
+}
